31d6caefa2c1d4022fddbc193871aaf276b69b49
dev <dev@debian-vbx.(none)>
2013-04-18 15:30:30 +0400

    rewrote flags init. fixed -d sort bug

--- tmp.c
+++ tmp.c
@@ -13,7 +13,6 @@
 #include <fcntl.h>
 #include <ctype.h>
 
-
 #define MAXLINES 100
 #define MAXLEN 100
 #define UPTOLOWDIFF 32
@@ -21,27 +20,31 @@
 char *lineptr[MAXLINES];
 int dflag = 0;
 int fflag = 0;
-
+int numeric = 0;
+int reverse = 0;
 
 int equalfold(char *s, char *t);
 void _swap(void *v[], int i, int j);
-int directory(char *s) {
-	if(!(*s)) return -1;
+int
+directory(char *s)
+{
+	if (!(*s))
+		return -1;
 	if (isdigit(*s) || isalpha(*s) || isspace(*s))
 		return 1;
 	return 0;
 }
 
-
-int myStrcmp(char *s, char *t) {
+int
+myStrcmp(char *s, char *t)
+{
 	while (*s && *t) {
 		if (dflag) {
-			if (!directory(s)) {
-				s++;
-				continue;
-			}
-			if (!directory(t)) {
-				t++;
+			if (!directory(s) || !directory(t)) {
+				if (!directory(s))
+					s++;
+				if (!directory(t))
+					t++;
 				continue;
 			}
 		}
@@ -55,15 +58,16 @@ int myStrcmp(char *s, char *t) {
 	return *s - *t;
 }
 
-int myStrcmpf(char *s, char *t) {
+int
+myStrcmpf(char *s, char *t)
+{
 	while (*s && *t) {
 		if (dflag) {
-			if (!directory(s)) {
-				s++;
-				continue;
-			}
-			if (!directory(t)) {
-				t++;
+			if (!directory(s) || !directory(t)) {
+				if (!directory(s))
+					s++;
+				if (!directory(t))
+					t++;
 				continue;
 			}
 		}
@@ -74,17 +78,18 @@ int myStrcmpf(char *s, char *t) {
 		s++;
 		t++;
 	}
-	char l,k;
+	char l, k;
 	l = *s;
 	k = *t;
-	if(fflag)
-	{
-		return (tolower(l)-tolower(k));
+	if (fflag) {
+		return (tolower(l) - tolower(k));
 	}
-	return l-k;
+	return l - k;
 }
 
-int _getline(char s[], int lim) {
+int
+_getline(char s[], int lim)
+{
 	int c, i;
 	for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
 		s[i] = c;
@@ -96,7 +101,9 @@ int _getline(char s[], int lim) {
 	return i;
 }
 
-int readlines(char *lineptr[], int maxlines) {
+int
+readlines(char *lineptr[], int maxlines)
+{
 	int len, nlines;
 	char *p, line[MAXLEN];
 	nlines = 0;
@@ -104,83 +111,64 @@ int readlines(char *lineptr[], int maxlines) {
 		if (nlines >= MAXLINES || (p = malloc(len)) == NULL)
 			return -1;
 		else {
-			line[len - 1] = '\0'; /* delete newline */
+			line[len - 1] = '\0';
 			strcpy(p, line);
 			lineptr[nlines++] = p;
 		}
 	return nlines;
 }
 
-void writelines(char *lineptr[], int nlines) {
+void
+writelines(char *lineptr[], int nlines)
+{
 	int i;
 	for (i = 0; i < nlines; i++)
 		printf("%s\n", lineptr[i]);
 }
 
-void reverselines(char *lineptr[], int nlines) {
+void
+reverselines(char *lineptr[], int nlines)
+{
 	int i;
 	for (i = 0; i < nlines / 2; i++)
-		_swap((void **)lineptr, i, nlines - i - 1);
+		_swap((void **) lineptr, i, nlines - i - 1);
 }
 
-/*long long hashcodef(char *s1) {
-	long long v1 = 0;
-	long long i = 1;
-	while (*s1) {
-		if (dflag) {
-			if (!directory(s1))
-			continue;
-		}
-		if (islower(*s1))
-			v1 += ((*s1 - 32) * i) % 1000000000;
-		else
-			v1 += (*s1 * i) % 1000000000;
-		i *= 13;
-		i %= 1000000000;
-		s1++;
-	}
-	return v1;
-}
-
-long long hashcode(char *s1) {
-	long long v1 = 0;
-	long long i = 1;
-	while (*s1) {
-		if (dflag) {
-			if (!directory(s1))
-			continue;
-		}
-		v1 += (*s1 * i) % 1000000000;
-		i *= 13;
-		i %= 1000000000;
-		s1++;
-	}
-	return v1;
-}*/
-
-void drop_error_string_format(){
-	fprintf(stderr,"Incomparable string to numerical sorting\n");
+void
+drop_error_string_format()
+{
+	fprintf(stderr, "Incomparable string to numerical sorting\n");
 	exit(0);
 }
 
-int check_string_format(char *s)
+int
+check_string_format(char *s)
 {
-	if(!(*s)) return -1;
-	while(isdigit(*s)) s++;
-	if(*s) return 0;
+	if (!(*s))
+		return -1;
+	if (*s == '-' || *s == '+' || isdigit(*s))
+		s++;
+	else
+		return 0;
+	while (isdigit(*s))
+		s++;
+	if (*s)
+		return 0;
 	return 1;
 }
 
-int numcmp(char *s1, char *s2) {
+int
+numcmp(char *s1, char *s2)
+{
 	long double v1, v2;
-	if(!(*s1) || !(*s2)) return 300;
-	int h=check_string_format(s1);
-	int g=check_string_format(s2);
-	if(!h || !g) 
-	{
-		#ifndef TEST 
+	if (!(*s1) || !(*s2))
+		return 300;
+	int h = check_string_format(s1);
+	int g = check_string_format(s2);
+	if (!h || !g) {
+#ifndef TEST
 		drop_error_string_format();
-		#endif
+#endif
 		return 300;
 	}
 	v1 = atof(s1);
@@ -193,58 +181,85 @@ int numcmp(char *s1, char *s2) {
 		return 0;
 }
 
-
-int equalfold(char *s, char *t) {
-	if(!(*s) || !(*t)) return -1;
-	if(*s==*t) return 1;
+int
+equalfold(char *s, char *t)
+{
+	if (!(*s) || !(*t))
+		return -1;
+	if (*s == *t)
+		return 1;
 	if (abs(*s - *t) == UPTOLOWDIFF && isalpha(*s) && isalpha(*t))
 		return 1;
 	return 0;
 }
 
-void destroy(void *v[], int n) {
+void
+destroy(void *v[], int n)
+{
 	int i;
 	for (i = 0; i < n; i++)
 		free(v[i]);
 }
 
-void _swap(void *v[], int i, int j) {
+void
+_swap(void *v[], int i, int j)
+{
 	void *temp;
 	temp = v[i];
 	v[i] = v[j];
 	v[j] = temp;
 }
 
-void _qsort(void *v[], int left, int right, int(*comp)(void *, void *)) {
+void
+__qsort(void *v[], int left, int right, int(*comp)(void *, void *))
+{
 	int i, last;
 	void _swap(void *v[], int, int);
-	if (left >= right) /* do nothing if array contains */
-		return; /* fewer than two elements */
+	if (left >= right)
+		return;
 	_swap(v, left, (left + right) / 2);
 	last = left;
 	for (i = left + 1; i <= right; i++)
 		if ((*comp)(v[i], v[left]) < 0)
 			_swap(v, ++last, i);
 	_swap(v, left, last);
-	_qsort(v, left, last - 1, comp);
-	_qsort(v, last + 1, right, comp);
+	__qsort(v, left, last - 1, comp);
+	__qsort(v, last + 1, right, comp);
 }
+
+void
+_qsort(void *v[], int left, int right, int(*comp)(void *, void *))
+{
+	__qsort(v, 0, right, comp);
+	if (reverse)
+		reverselines((void *) v, right + 1);
+}
+
 #ifndef TEST
 int main(int argc, char *argv[]) {
-	int nlines; /* number of input lines read */
-	int numeric = 0; /* 1 if numeric sort */
-	int reverse = 0; /*1 for reverse sort*/
-	int k;
-	for (k = 1; k < argc; k++) {
-		if (strcmp(argv[k], "-n") == 0)
-			numeric = 1;
-		if (strcmp(argv[k], "-r") == 0)
-			reverse = 1;
-		if (strcmp(argv[k], "-f") == 0)
-			fflag = 1;
-		if (strcmp(argv[k], "-d") == 0)
-			dflag = 1;
-	}
+	int nlines;
+	char c;
+	int k = 0;
+	while (--argc > 0 && (*++argv)[k] == '-')
+		while ((++argv[k]) && (c = *argv[k]))
+			switch (c) {
+			case 'd':
+				dflag = 1;
+				break;
+			case 'f':
+				fflag = 1;
+				break;
+			case 'r':
+				reverse = 1;
+				break;
+			case 'n':
+				numeric = 1;
+				break;
+			default:
+				printf("warning: illegal option %c\n", c);
+				argc = 0;
+				break;
+			}
 	if ((nlines = readlines(lineptr, MAXLINES)) >= 0) {
 		if (fflag) {
 			_qsort((void**) lineptr, 0, nlines - 1,
@@ -252,8 +267,6 @@ int main(int argc, char *argv[]) {
 		} else
 			_qsort((void**) lineptr, 0, nlines - 1,
 					(int(*)(void*, void*)) (numeric ? numcmp : myStrcmp));
-		if (reverse)
-			reverselines(lineptr, nlines);
 		writelines(lineptr, nlines);
 		destroy((void**) lineptr, nlines);
 		return 0;
